56298eff67c080ab987055ac34bffeec5abbb6ac
XuanAnh1995 <luuvietxuananh95@gmail.com>
2025-06-29 06:16:15 +0700

    update name file ngoctrinh-coffee to ngoctrinh-coffee-redirect

---
+++ Session07-SpringMVC/ngoctrinh-coffee-redirect/src/test/java/com/luuviet/ngoctrinhcoffee/NgoctrinhCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luuviet.ngoctrinhcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoctrinhCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
